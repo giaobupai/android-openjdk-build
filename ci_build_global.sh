eb3b69f84f0aace2c58054c8b4aa943c46f3b508
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-02-09 21:53:37 +0800

    Update ci_build_global.sh

--- ci_build_global.sh
+++ ci_build_global.sh
@@ -6,9 +6,8 @@ export JDK_DEBUG_LEVEL=release
 
 if [[ "$BUILD_IOS" != "1" ]]; then
 
-  wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux-x86_64.zip"
+  wget -nc -nv -O android-ndk-$NDK_VERSION-linux.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux.zip"
   ./extractndk.sh
-  ./maketoolchain.sh
 else
   chmod +x ios-arm64-clang
   chmod +x ios-arm64-clang++
