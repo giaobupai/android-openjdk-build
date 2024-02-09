1436aec80ad1ec80f3cfbe969e13ba726468a6af
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-02-10 00:41:02 +0800

    Update getlibs.sh

--- getlibs.sh
+++ getlibs.sh
@@ -4,6 +4,6 @@
 
 wget https://downloads.sourceforge.net/project/freetype/freetype2/$BUILD_FREETYPE_VERSION/freetype-$BUILD_FREETYPE_VERSION.tar.gz
 tar xf freetype-$BUILD_FREETYPE_VERSION.tar.gz
-wget https://github.com/OpenPrinting/cups/releases/download/v2.4.2/cups-2.4.2-source.tar.gz
-tar xf cups-2.4.2-source.tar.gz
-rm cups-2.4.2-source.tar.gz freetype-$BUILD_FREETYPE_VERSION.tar.gz
+wget https://github.com/OpenPrinting/cups/releases/download/v2.4.7/cups-2.4.7-source.tar.gz
+tar xf cups-2.4.7-source.tar.gz
+rm cups-2.4.7-source.tar.gz freetype-$BUILD_FREETYPE_VERSION.tar.gz
