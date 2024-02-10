0f6262ffda99d545ea8f9735e787a941c2c2d95b
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-02-10 19:44:34 +0800

    Create maketoolchain.sh

---
+++ maketoolchain.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+. setdevkitpath.sh
+
+$NDK/build/tools/make-standalone-toolchain.sh \
+	--arch=${TARGET_SHORT} \
+	--platform=android-21 \
+	--install-dir=$NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
+cp devkit.info.${TARGET_SHORT} $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain/
