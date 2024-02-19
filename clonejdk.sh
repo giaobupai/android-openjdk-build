d66363eb50c9abb70838e83d3256694f1926c310
giaobupai <137373766+giaobupai@users.noreply.github.com>
2024-02-20 00:56:55 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-git clone -b jdk-17.0.10+6_adopt --depth 1 https://github.com/adoptium/jdk17u openjdk
+git clone --depth 1 https://github.com/adoptium/jdk21u openjdk
