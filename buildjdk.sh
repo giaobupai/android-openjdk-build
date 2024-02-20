8eb162ab9f2c84e8aa395b25dee29ec866e4c567
giaobupai <137373766+giaobupai@users.noreply.github.com>
2024-02-20 16:35:17 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 export FREETYPE_DIR=$PWD/freetype-$BUILD_FREETYPE_VERSION/build_android-$TARGET_SHORT
 export CUPS_DIR=$PWD/cups-2.4.7
-export CFLAGS+=" -DLE_STANDALONE -DANDROID -pipe -integrated-as -fno-plt -Ofast -flto -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce" # -I"$FREETYPE_DIR" -I"$CUPS_DI"
+export CFLAGS+=" -DLE_STANDALONE"
 if [[ "$TARGET_JDK" == "arm" ]]
 then
   export CFLAGS+=" -D__thumb__"
